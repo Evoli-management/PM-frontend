9f4019ca1b3e48a599e753644ed0077d5ad56d18
Hussein R Ngapawa <husseinramdin123@gmail.com>
2025-08-18 10:33:06 +0300

    deploy_through_gitPages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/PM-frontend/', // ✅ This is required for GitHub Pages
   plugins: [react(), tailwindcss()],
 })
