33ed27b1c19ad331ae9272d10c173375c80f1b49
DAMIAN-GALLUSI <damiangalusi123@gmail.com>
2025-08-18 15:45:58 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
 import Navbar from "./components/shared/Navbar.jsx";
 import Footer from "./components/shared/Footer.jsx";
-<<<<<<< HEAD
-import SetProfile from "./pages/SetProfile.jsx";
-import PasswordPageForget from "./pages/PasswordPageForget.jsx";
-import Pricing from "./pages/Pricing.jsx";
-
-=======
 import Home from "./pages/Home.jsx";
-import LoginPage from "./pages/Login.jsx";
->>>>>>> aeea56a03f3752720ebd5aed7ef24813c536edda
-
 export default function App() {
     return (
         <>
             <Navbar />
-<<<<<<< HEAD
              <Home />
-            <SetProfile />
-            <PasswordPageForget />
-            <ResetPasswordpage />
-            <Login />
-
-
-=======
-            <Home />
-            <LoginPage />
->>>>>>> aeea56a03f3752720ebd5aed7ef24813c536edda
             <Footer />
         </>
     )
