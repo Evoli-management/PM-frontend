aeea56a03f3752720ebd5aed7ef24813c536edda
costa-sanga <costavicton@gmail.com>
2025-08-18 14:21:18 +0300

    route1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/shared/Navbar.jsx";
 import Footer from "./components/shared/Footer.jsx";
 import Home from "./pages/Home.jsx";
+import LoginPage from "./pages/Login.jsx";
 
 export default function App() {
     return (
         <>
             <Navbar />
             <Home />
+            <LoginPage />
             <Footer />
         </>
     )
