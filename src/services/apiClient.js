59c0e37b86ea78ff22585804b6d7b8be1969f21f
Hussein R Ngapawa <husseinramdin123@gmail.com>
2025-09-05 10:26:14 +0200

    .env file configuration

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const apiBase = import.meta.env.DEV
     ? "/api" // use Vite proxy in development for same-origin cookies
-    : import.meta.env.VITE_API_URL || "/api";
+    : import.meta.env.VITE_API_BASE_URL || "/api";
 
 const apiClient = axios.create({
     baseURL: apiBase,
