bc9923320765ee6b16b6a7616b8709296b78e46d
costa-victon <costa-victon@users.noreply.github.com>
2025-12-10 15:20:05 +0000

    deploy: 78f769d2b8105ce5f4df262c216326b91dcebd67

--- assets/GoalForm-DIh514qd.js
+++ assets/GoalForm-DIh514qd.js
@@ -1,4 +1,4 @@
-import{c as F,r as o,j as e,C as T,P as z,T as E}from"./index-BxB8o1Aq.js";/**
+import{c as F,r as o,j as e,C as T,P as z,T as E}from"./index-Cp80VrkK.js";/**
  * @license lucide-react v0.511.0 - ISC
  *
  * This source code is licensed under the ISC license.
