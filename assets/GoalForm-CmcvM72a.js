3b1307b8fe98a0f255c0b68f16f9faf202be76cf
costa-victon <costa-victon@users.noreply.github.com>
2025-12-31 08:30:15 +0000

    deploy: 175f807179ad44fdc83d9a6d77c68d52633507e4

--- assets/GoalForm-CmcvM72a.js
+++ assets/GoalForm-CmcvM72a.js
@@ -1,4 +1,4 @@
-import{c as O,r as l,j as t,C as U,T as G}from"./index-Bx9MfAEF.js";/**
+import{c as O,r as l,j as t,C as U,T as G}from"./index-HzxuzZ70.js";/**
  * @license lucide-react v0.511.0 - ISC
  *
  * This source code is licensed under the ISC license.
