cab5d5a68e0fb97dad900bf3a80f8ed2ef0322a5
husseinramdin <husseinramdin@users.noreply.github.com>
2026-01-03 12:30:28 +0000

    deploy: 8253b19035d532912e61d563e883be7d0b069436

--- assets/GoalForm-Ci2fztaI.js
+++ assets/GoalForm-Ci2fztaI.js
@@ -1,4 +1,4 @@
-import{c as O,r as l,j as t,C as U,T as G}from"./index-BJSpUB4W.js";/**
+import{c as O,r as l,j as t,C as U,T as G}from"./index-BXD01EXl.js";/**
  * @license lucide-react v0.511.0 - ISC
  *
  * This source code is licensed under the ISC license.
